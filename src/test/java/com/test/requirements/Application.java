2ec1635b28b47d91d6015029edd9eadd3b9fd832
kesha <kuravskiy@gmail.com>
2013-09-09 16:38:32 +0200

    added simple thucydides project
    build and start: mvn install
    result report: target/site/index.html

---
+++ src/test/java/com/test/requirements/Application.java
@@ -0,0 +1,11 @@
+package com.test.requirements;
+
+import net.thucydides.core.annotations.Feature;
+
+public class Application {
+    @Feature
+    public class Search {
+        public class SearchByKeyword {}
+        public class SearchByMultipleKeywords {}
+    }
+}
